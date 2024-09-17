7154ad07d3293ac34e955b999efc551aa221b229
Esley Ferreira <140576056+EsleyFerreira@users.noreply.github.com>
2024-09-17 20:42:01 -0300

    Update App.js

--- App.js
+++ App.js
@@ -277,6 +277,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#FFE2C4',
   },
   splashImage: {
     width: 200,
